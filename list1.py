665cce0c5ce1602c4f18bb0f31bd76d4dfb27ae5
smallsa <2806011959@qq.com>
2021-01-26 21:42:45 +0800

    列表首次练习

---
+++ list1.py
@@ -0,0 +1,7 @@
+names = ['jiaojiao', 'lele', 'yuyu']
+traffic = ['bus', 'car', 'motorcycle']
+for i in range(3):
+    print(names[i])
+    print('Hello'+' '+names[i])
+for j in range(3):
+    print('I would like to own a'+' '+traffic[j])
